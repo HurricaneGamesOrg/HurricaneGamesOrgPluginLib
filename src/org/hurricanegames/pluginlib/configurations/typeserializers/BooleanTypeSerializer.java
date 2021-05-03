86984de951a3f7cb6e07574f9699a7eb94e952c5
Shevchik <Shev4ik.den@gmail.com>
2021-05-04 01:23:58 +0300

    BooleanTypeSerializer static instance

--- src/org/hurricanegames/pluginlib/configurations/typeserializers/BooleanTypeSerializer.java
+++ src/org/hurricanegames/pluginlib/configurations/typeserializers/BooleanTypeSerializer.java
@@ -2,6 +2,8 @@ package org.hurricanegames.pluginlib.configurations.typeserializers;
 
 public class BooleanTypeSerializer implements TypeSerializer<Boolean> {
 
+	public static final BooleanTypeSerializer INSTANCE = new BooleanTypeSerializer();
+
 	@Override
 	public Boolean deserialize(Object object) {
 		if (object instanceof Boolean) {
